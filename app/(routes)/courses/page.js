1a732221c4a22e2f73e8cc867320fa211f52a5db
Suryapratap Biswal <suryapratapbiswal009@gmail.com>
2024-02-16 09:34:08 +0530

    completed navbar , sidebar  and banner

--- app/(routes)/courses/page.js
+++ app/(routes)/courses/page.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import WelcomeBanner from './_componnts/WelcomeBanner'
 
 const Courses
     = () => {
         return (
-            <div>Courses
-
+            <div className='grid grid-cols-1 md:grid-cols-3 p-5'>
+            {/* Left Cointainer */}
+                <div className='col-span-2'>
+                    {/* Banner */}
+                    <WelcomeBanner/>
+                </div>
+            {/* Right Cointainer */}
+                <div>
+                    Right section
+                </div>
             </div>
         )
     }
